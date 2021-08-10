54d25e9908545cc95b3be9a2d268b2f4830f5fd1
Damian Czechowski <d.czechowski92@gmail.com>
2021-08-10 18:28:39 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 function App () {
+  console.log('test')
   return (
     <Provider store={store}>
       <GameContainer />
+
     </Provider>
   );
 }
